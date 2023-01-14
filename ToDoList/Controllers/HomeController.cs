e83c3d743934477e518b62c1461b5ec591481ee5
Ali AYTEKİN <96261457+aytek55@users.noreply.github.com>
2023-01-14 18:07:29 +0300

    TodoList

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -3,28 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Listele()
         {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            ViewBag.Liste = db.Lists;
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Kaydet(List l)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            db.Lists.Add(l);
+            db.SaveChanges();
+            return RedirectToAction("Listele");
+        }
+        [HttpPost]
+        public ActionResult Duzenle(List D)
+        {
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            List KL = db.Lists.FirstOrDefault(x => x.ID == D.ID);
+            KL.Title = D.Title;
+            KL.Description = D.Description;
+            db.SaveChanges();
+            return RedirectToAction("Listele");
         }
 
-        public ActionResult Contact()
+        public ActionResult Sil(int ID)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            ToDoListEntitiesConnectionStringDB db = new ToDoListEntitiesConnectionStringDB();
+            List silinecek = db.Lists.FirstOrDefault(x => x.ID == ID);
+            db.Lists.Remove(silinecek);
+            db.SaveChanges();
+            return RedirectToAction("Listele");
         }
     }
 }
